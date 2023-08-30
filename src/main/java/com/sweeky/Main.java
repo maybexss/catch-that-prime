2ef35a623423312b6437184c01185a8b3f62bccb
maybexss <278567131@qq.com>
2023-08-30 20:34:12 +0800

    init

---
+++ src/main/java/com/sweeky/Main.java
@@ -0,0 +1,11 @@
+package com.sweeky;
+
+/**
+ * @author 4freedom
+ * @Date 2023/8/30 20:30
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("ok");
+    }
+}
